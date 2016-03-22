207fb9912c68407466eaa7f113b71c5a337c1547
Henry <Henry@10.191.0.2>
2016-03-22 08:43:33 +0800

    @JsonProperty can co-exist with and overrides the "CREATOR"

--- learn_jackson/src/test/java/org/henry/jackson/JsonAutoDetectTest.java
+++ learn_jackson/src/test/java/org/henry/jackson/JsonAutoDetectTest.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.annotate.JsonAutoDetect;
 import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonMethod;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -17,24 +19,39 @@ public class JsonAutoDetectTest
 	@Test
 	public void testJsonAutoDetectWithMethodCreator() throws JsonParseException, JsonMappingException, IOException
 	{
-		String json = "{\"age\":18, \"name\":\"John\"}";
+		String json = "{\"age\":18, \"name\":\"John\", \"email\":\"john.smith@gmail.com\"}";
 		ObjectMapper om = new ObjectMapper();
 		User u = om.readValue(json, User.class);
 		Assert.assertNotNull(u);
 		Assert.assertNotNull(u.name);
+		Assert.assertEquals(u.age, 18);
+		// @JsonProperty can co-exist with and overrides the "CREATOR"
+		Assert.assertNotNull(u.email);
 	}
 	
 	@JsonAutoDetect(value=JsonMethod.CREATOR)
+	// @JsonIgnoreProperties(ignoreUnknown=true)
 	private static class User
 	{
 		private int age;
 		private String name;
+		@JsonProperty
+		private String email;
 		
 		@JsonCreator
 		public User(@JsonProperty("age") int age, @JsonProperty("name") String name) {
 			this.age = age;
 			this.name = name;
 		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
 		
 	}
+	
 }
